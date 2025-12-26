60467a1edb097592f5e7fcbf1a8ea324acb9f301
kaviselvam-28 <kaviselvam028@gmail.com>
2025-12-26 19:05:40 +0530

    C_Problems

---
+++ C_Problems_(Assessment03)_(Problem05).c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main ()
+{
+    int a;
+    printf("Enter the num:");
+    scanf("%d",&a);
+    if(a<=50)
+        printf("success");
+    else
+        printf("Failure");
+}
