b8e0079d850410de5fd6b0bd00157652dadd9723
kaviselvam-28 <kaviselvam028@gmail.com>
2026-01-05 21:32:16 +0530

    C_Problems

---
+++ C_Problems_(Assessment04)_(Problem07).c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int x,a,b,sum;
+    x=10;
+    sum=0;
+    Loop:if(x<99)
+    {
+        a=x%10;
+        b=x/10;
+        sum=a+b;
+        if((sum==7) && (x % 2 != 0) )
+        {
+            printf("%d\n",x);
+        }
+        x++;
+        goto Loop;
+    }
+}
