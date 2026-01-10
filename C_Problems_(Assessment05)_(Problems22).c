16292c796009c7e70094c4fc09eeb28abc7b6a3f
kaviselvam-28 <kaviselvam028@gmail.com>
2026-01-10 21:49:42 +0530

    C_Problems

---
+++ C_Problems_(Assessment05)_(Problems22).c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int a,d,count=0;
+    printf("Enter the num :");
+    scanf("%d",&a);
+    for(;a>10;a/=10)
+    {
+        d = a % 100;
+        if(d % 2 != 0)
+        {
+            count++;
+        }
+    }
+    printf("%d",count);
+}
