1136ffc9a03de0d86e4938937f091aea3c1e42b5
kaviselvam-28 <kaviselvam028@gmail.com>
2026-01-26 22:13:15 +0530

    Create C_Problems_(Assessment07)_(Problems02).c

---
+++ C_Problems_(Assessment07)_(Problems02).c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void disp_descend(int);
+int main ()
+{
+    int x,y;
+    scanf("%d",&x);
+    disp_descend(x);
+}
+void disp_descend(int x) 
+{
+    int i;
+    i=x;
+    for (;i>=1;i--)
+    {
+        printf ("%d\n",i);
+    }
+}
